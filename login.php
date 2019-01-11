1ec93b5f00cbe375bbfe8086257c10b9f9a4a544
pilz-kornelia <44549008+pilz-kornelia@users.noreply.github.com>
2019-01-11 18:49:32 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,31 +1,33 @@
-<?php include('server.php') ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+
+  </head>
+  <body>
+    <?php require_once 'process.php'; ?>
+   <div class="row justify-content-center">
+      <form action="process.php" method="POST">
+    <div class="form-group">
+      <label>Name</label>
+        <input type="text" name="name" class="form-control" value="Enter your name">
+    </div>
+    <div class="form-group">
+        <label>Location</label>
+        <input type="text" name="location" class="form-control"  value="Enter your location">
+    </div>
+    <div class="form-group">
+        <button type="submit" name="save">Save</button>
+    </div>
+      </form>
+    </div>  
+  </body>
 </html>
